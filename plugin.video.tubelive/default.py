c40667a34c05fec1b8f4f2bf19d4fbacbe41d82b
husham memar <website@husham.com>
2017-01-02 20:11:14 +0000

    new commit

--- plugin.video.tubelive/default.py
+++ plugin.video.tubelive/default.py
@@ -14,14 +14,14 @@ import plugintools
 import xbmc,xbmcaddon
 from addon.common.addon import Addon
 
-addonID = 'plugin.video.tubelive'
+addonID = 'plugin.video.spinninrecords'
 addon = Addon(addonID, sys.argv)
 local = xbmcaddon.Addon(id=addonID)
 icon = local.getAddonInfo('icon')
 
 YOUTUBE_CHANNEL_ID_1 = "PLU12uITxBEPEoCeVvq344LJGtNQxrqyTJ"
-
-
+YOUTUBE_CHANNEL_ID_2 = "PL8fVUTBmJhHIegh7kgk6KsyLJ7miRBKpO"
+YOUTUBE_CHANNEL_ID_3 = "PL57quI9usf_uLyWHLNz5yGH2qfbn8HMFo"
 
 
 
@@ -46,10 +46,25 @@ def main_list(params):
 
     plugintools.add_item( 
         #action="", 
-        title="Live",
-        url="plugin://plugin.video.youtube/playlist/"+YOUTUBE_CHANNEL_ID_1+"/",
+        title="live Most Vieved",
+         url="plugin://plugin.video.youtube/playlist/"+YOUTUBE_CHANNEL_ID_1+"/",
+        thumbnail="https://yt3.ggpht.com/BQSIiLDxZiZpzR4C6pK-NKkKOv3fsPsWuKVJxpmYCnEASI8W5pglXP1vF30D98doHzsP_JwPLycrfd-FFnw=s100-nd-c-c0xffffffff-rj-k-no",
+        folder=True )  
+
+
+    plugintools.add_item( 
+        #action="", 
+        title="Live Now  Sports",
+        url="plugin://plugin.video.youtube/playlist/"+YOUTUBE_CHANNEL_ID_2+"/",
+        thumbnail="https://yt3.ggpht.com/BQSIiLDxZiZpzR4C6pK-NKkKOv3fsPsWuKVJxpmYCnEASI8W5pglXP1vF30D98doHzsP_JwPLycrfd-FFnw=s100-nd-c-c0xffffffff-rj-k-no",
+        folder=True )
+
+    plugintools.add_item( 
+        #action="", 
+        title="Live Now Technology",
+        url="plugin://plugin.video.youtube/playlist/"+YOUTUBE_CHANNEL_ID_3+"/",
         thumbnail="https://yt3.ggpht.com/BQSIiLDxZiZpzR4C6pK-NKkKOv3fsPsWuKVJxpmYCnEASI8W5pglXP1vF30D98doHzsP_JwPLycrfd-FFnw=s100-nd-c-c0xffffffff-rj-k-no",
         folder=True )
 		
-		    
+		
 run()
