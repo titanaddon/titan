de90128cac00f359604633d44919c0144992c372
husham memar <website@husham.com>
2017-01-08 17:39:50 +0000

    new commit

--- plugin.video.DocumentaryTV/default.py
+++ plugin.video.DocumentaryTV/default.py
@@ -19,7 +19,7 @@ addon = Addon(addonID, sys.argv)
 local = xbmcaddon.Addon(id=addonID)
 icon = local.getAddonInfo('icon')
 
-YOUTUBE_CHANNEL_ID_1 = "UCcO4t8Q0fWrwt7taToZsyFg"
+YOUTUBE_CHANNEL_ID_1 = "PLJ-qZvjnxPlRhvwZemI5feMj6wxViIU11"
 YOUTUBE_CHANNEL_ID_2 = "UCVB54sxE7pliAJYxeRbiz7A"
 
 
@@ -45,15 +45,15 @@ def main_list(params):
     plugintools.add_item( 
         #action="", 
         title="Documentary TV",
-        url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_1+"/",
-        thumbnail="https://yt3.ggpht.com/-G09Iq74Fs-I/AAAAAAAAAAI/AAAAAAAAAAA/bH_puDCzDY4/s100-c-k-no-mo-rj-c0xffffff/photo.jpg",
+        url="plugin://plugin.video.youtube/playlist/"+YOUTUBE_CHANNEL_ID_1+"/",
+        thumbnail="https://i.imgsafe.org/ac591a33f8.jpg",
         folder=True )
 
     plugintools.add_item( 
         #action="", 
         title="National Geographic",
         url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_2+"/",
-        thumbnail="https://i.ytimg.com/vi/KpDghmRR68Y/hqdefault.jpg?custom=true&w=320&h=180&stc=true&jpg444=true&jpgq=90&sp=68&sigh=Mmj3gqqb4G-Zuacxiyw0Eb5iYiY",
+        thumbnail="https://i.imgsafe.org/ac591a33f8.jpg",
         folder=True )
 
     
